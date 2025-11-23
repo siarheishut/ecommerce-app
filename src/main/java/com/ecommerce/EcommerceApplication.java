1a172eab1915fc203ba7d18210bf2a7416e9b6c4
siarheishut <szut.siergiej@gmail.com>
2025-11-23 14:02:11 +0100

    Set default application time zone to Europe/Warsaw
    Add initialization method in `EcommerceApplication` to explicitly set the default timezone to "Europe/Warsaw"

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -1,13 +1,21 @@
 package com.ecommerce;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableAsync
 public class EcommerceApplication {
   public static void main(String[] args) {
     SpringApplication.run(EcommerceApplication.class, args);
+  }
+
+  @PostConstruct
+  public void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Europe/Warsaw"));
   }
 }
